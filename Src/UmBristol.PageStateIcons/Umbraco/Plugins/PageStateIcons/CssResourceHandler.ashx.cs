6e73f414d0a8895b637c970b4590ec0004912224
Adam <Adam@Adam-Laptop.aurosltd.local>
2012-07-08 14:41:46 +0100

    Tidyup of css rules handler

--- Src/UmBristol.PageStateIcons/Umbraco/Plugins/PageStateIcons/CssResourceHandler.ashx.cs
+++ Src/UmBristol.PageStateIcons/Umbraco/Plugins/PageStateIcons/CssResourceHandler.ashx.cs
@@ -6,8 +6,16 @@ using System.Web;
 
 namespace UmBristol.PageStateIcons
 {
-    class CssResourceHandler : IHttpHandler
+    /// <summary>
+    ///   Dynamically generates the CSS to be used for applying icon overlays to the Umbraco page tree.
+    /// </summary>
+    public class CssResourceHandler : IHttpHandler
     {
+        /// <summary>
+        ///   Gets the starting z index for the custom overlays.
+        /// </summary>
+        private static readonly int ZIndexStartingIndex = 600;
+
         public bool IsReusable
         {
             get { return false; }
@@ -15,27 +23,30 @@ namespace UmBristol.PageStateIcons
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/css";
+            int zindex = ZIndexStartingIndex;
 
             StringBuilder sb = new StringBuilder();
-
-            int zIndex = 600;
-
+            
+            // Enumerate the rules to generate the CSS which is required for the overlap images.
             foreach (Config.RuleElement rule in Config.PageStateIconsConfigurationSection.Instance.Rules)
             {
-                zIndex += 1;
+                zindex += 1;
 
                 sb.AppendFormat(".overlay-{0} ", rule.Name);
                 sb.Append("{");
                 sb.AppendFormat("left: {0}px; ", rule.Left);
                 sb.AppendFormat("top: {0}px; ", rule.Top);
                 sb.AppendFormat("background: no-repeat url(\"{0}\") 0 0; ", rule.OverlayIconPath);
-                sb.AppendFormat("z-index: {0}", zIndex);
+                sb.AppendFormat("z-index: {0}", zindex);
                 sb.Append("}");
                 sb.AppendLine(string.Empty);
             }
 
+            context.Response.ContentType = "text/css";
+
             context.Response.Write(sb.ToString());
+
+            context.Response.End();
         }
     }
 }
