fed20dc6fd90f986a1bbe4f57251f6ac203736b1
Adam <Adam@Adam-Laptop.aurosltd.local>
2012-07-08 13:54:48 +0100

    Added top and left attributes to xml config

---
+++ Src/UmBristol.PageStateIcons/Umbraco/Plugins/PageStateIcons/CssResourceHandler.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace UmBristol.PageStateIcons
+{
+    class CssResourceHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/css";
+
+            StringBuilder sb = new StringBuilder();
+
+            int zIndex = 600;
+
+            foreach (Config.RuleElement rule in Config.PageStateIconsConfigurationSection.Instance.Rules)
+            {
+                zIndex += 1;
+                
+                sb.AppendFormat(".overlay-{0} ", rule.Name);
+                sb.Append("{");
+                sb.AppendFormat("left: {0}px; ", rule.Left);
+                sb.AppendFormat("top: {0}px; ", rule.Top);
+                sb.AppendFormat("background: no-repeat url(\"{0}\") 0 0; ", rule.OverlayIconPath);
+                sb.AppendFormat("z-index: {0}", zIndex);
+                sb.Append("}");
+                sb.AppendLine(string.Empty);
+            }
+
+            context.Response.Write(sb.ToString());
+        }
+    }
+}
