83e1eecf39c112eb95240c93cfbf5c4a7b29680d
Lee Kelleher <leekelleher@users.noreply.github.com>
2012-07-08 17:00:29 +0100

    Bug fixes! woo

---
+++ Src/UmBristol.PageStateIcons/Application.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using umbraco.BusinessLogic;
+using umbraco.cms.presentation.Trees;
+using umbraco.NodeFactory;
+using System.Xml;
+using umbraco.cms.businesslogic.web;
+using UmBristol.PageStateIcons.Config;
+using umbraco.cms.helpers;
+
+[assembly: PreApplicationStartMethod(typeof(UmBristol.PageStateIcons.Application), "RegisterModules")]
+
+namespace UmBristol.PageStateIcons
+{
+	public class Application : ApplicationBase
+	{
+		private static bool modulesRegistered;
+
+		public Application()
+		{
+			BaseTree.BeforeNodeRender += new BaseTree.BeforeNodeRenderEventHandler(this.BaseTree_BeforeNodeRender);
+		}
+
+		public void BaseTree_BeforeNodeRender(ref XmlTree sender, ref XmlTreeNode node, EventArgs e)
+		{
+			if (node.NodeType != "content") return;
+
+			// try to get node from XML cache
+			XmlNode xmlNode = umbraco.content.Instance.XmlContent.GetElementById(node.NodeID);
+
+			if (xmlNode == null)
+			{
+				// if unpublished, get from Document (database)
+				var doc = new Document(int.Parse(node.NodeID));
+				if (doc != null)
+				{
+					// get the preview XML
+					xmlNode = doc.ToPreviewXml(new XmlDocument());
+				}
+			}
+
+			// if we have an XML node...
+			if (xmlNode != null)
+			{
+				// ... match rules
+				foreach (RuleElement rule in PageStateIconsConfigurationSection.Instance.Rules)
+				{
+					// apply XPath
+					var xpath = string.Concat("self::*[", rule.XPath, "]");
+					if (xmlNode.SelectSingleNode(xpath) != null)
+					{
+						// add custom class
+						node.Style.AddCustom(String.Format("overlay-{0}", Casing.SafeAlias(rule.Name)));
+					}
+				}
+			}
+		}
+
+		public static void RegisterModules()
+		{
+			if (modulesRegistered)
+				return;
+
+			DynamicModuleUtility.RegisterModule(typeof(UmBristol.PageStateIcons.Modules.RegisterClientResources));
+
+			modulesRegistered = true;
+		}
+	}
+}
---
+++ Src/UmBristol.PageStateIcons/Modules/RegisterClientResources.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web;
+using umbraco;
+using UmBristol.PageStateIcons.Filters;
+
+namespace UmBristol.PageStateIcons.Modules
+{
+	public class RegisterClientResources : IHttpModule
+	{
+		public void Dispose()
+		{
+		}
+
+		public void Init(HttpApplication context)
+		{
+			context.PostMapRequestHandler += new EventHandler(this.context_PostMapRequestHandler);
+		}
+
+		private void context_PostMapRequestHandler(object sender, EventArgs e)
+		{
+			var context = sender as HttpApplication;
+			var executionPath = context.Request.CurrentExecutionFilePath;
+			var umbracoPath = string.Concat(GlobalSettings.Path, "/");
+			var defaultAspx = string.Concat(umbracoPath, "default.aspx");
+			var umbracoAspx = string.Concat(umbracoPath, "umbraco.aspx");
+
+			if (executionPath.StartsWith(umbracoPath, StringComparison.CurrentCultureIgnoreCase))
+			{
+				if (executionPath.Equals(defaultAspx, StringComparison.InvariantCultureIgnoreCase))
+				{
+					// /umbraco/default.aspx forwards to umbraco.aspx with Server.Transfer, which doesn't work because it doesn't have the right page, i.e. umbraco.aspx
+					context.Response.Redirect("umbraco.aspx");
+					return;
+				}
+				else if (executionPath.Equals(umbracoAspx, StringComparison.InvariantCultureIgnoreCase))
+				{
+					context.Response.Filter = new InjectResources(context.Response.Filter);
+				}
+			}
+		}
+	}
+}
