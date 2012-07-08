f1cdf13d127d650849a7a3fc0fccef10b5692771
Lee Kelleher <leekelleher@users.noreply.github.com>
2012-07-08 10:59:13 +0100

    Initial commit; added files.

---
+++ Src/UmBristol.PageStateIcons/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("UmBristol.PageStateIcons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("umBristol Community")]
+[assembly: AssemblyProduct("UmBristol.PageStateIcons")]
+[assembly: AssemblyCopyright("Copyright \xa9 umBristol Community 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM components.
+// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F27C095F-56EA-4CE9-B35A-2CD732B5C591")]
+
+// Version information for an assembly consists of the following four values:
+// [Major].[Minor].[Build].[Revision]
+[assembly: AssemblyVersion("1.0.0.*")]
+// [assembly: AssemblyFileVersion("1.0.0.*")]
