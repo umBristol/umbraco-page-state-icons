5804e24b03412329a43797a58e172ca87f7aca9a
Adam <Adam@Adam-Laptop.aurosltd.local>
2012-07-08 13:39:58 +0100

    Added handler for css Added inclusion of config into web.config.

---
+++ Src/UmBristol.PageStateIcons/AppBase.cs
@@ -0,0 +1,26 @@
+﻿namespace UmBristol.PageStateIcons
+{
+    using System;
+    using umbraco.BusinessLogic;
+    using umbraco.cms.presentation.Trees;
+    using umbraco.NodeFactory;
+
+    public class AppBase :ApplicationBase
+    {
+        public AppBase()
+        {
+            BaseTree.BeforeNodeRender += BaseTree_BeforeNodeRender;
+        }
+
+        protected void BaseTree_BeforeNodeRender(ref XmlTree sender, ref XmlTreeNode node, EventArgs e)
+        {
+            if (node.NodeType != "content") return;
+
+            var contentNode = new Node(Convert.ToInt32(node.NodeID));
+
+            if (contentNode.Properties["umbracoNaviHide"] != null)
+                if (contentNode.Properties["umbracoNaviHide"].ToString() == "1")
+                    node.Style.AddCustom("overlay-hidden");
+        }
+    }
+}
